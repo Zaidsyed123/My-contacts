2c47787269accf150ed0e27a282c1086ce7be4c7
Zaidsyed123 <71397217+Zaidsyed123@users.noreply.github.com>
2022-12-21 09:10:51 -0700

    Change

--- main.js
+++ main.js
@@ -1,47 +1,81 @@
-// My Contacts Basic
+// My Contacts 
+let contacts = [ 
+  { name: 'John Doe', email: 'jdoe@gmail.com', phone: '780-123-4567' },
+  { name: 'Jane Doe', email: 'janedoe@gmail.com', phone: '780-234-5678'},
+  { name: 'Jennie Doe', email: 'jenniedoe@gmail.com', phone: '780-345-6789'},
+  { name: 'Sue Smith', email: 'suesmith@gmail.com', phone: '780-456-7890'},
+  { name: 'Sally Smith', email: 'sallysmith@gmail.com', phone: '780-567-8901'},
+  {name: 'Johnny Doe', email: 'jdoe@gmail.com', phone: '780-678-9012'},
+];
+document.getElementById("go-btn").addEventListener("click", goBtnClicked);
 
-// HTML Elements
-let goBtnEl = document.getElementById('go-btn');
-let menuEl = document.getElementById('menu');
-let outputEl = document.getElementById('output');
-
-// Go Btn - Menu Listener
-goBtnEl.addEventListener('click', goBtnHandler);
-
-function goBtnHandler() {
+function goBtnClicked() {
   // Get Menu Selection
-  let selection = menuEl.value;
+  let selection = document.getElementById("menu").value;
 
-  if (selection === 'display-all') {
-    displayContacts();
-  } else if (selection === 'add') {
-    addContact();
-  } else if (selection === 'remove') {
-    removeContact();
-  } else if (selection === 'display-name') {
-    displayByName();
-  } else if (selection === 'display-country') {
-    displayByCountry();
+  if (selection === "all") {
+    console.log("All Contacts");
+    displayAllContacts();
+  } else if (selection === "search") {
+    console.log("Search For Contacts");
+    SearchContacts();
+  } else if (selection === "edit") {
+    console.log("Edit Contacts");
+    EditContacts();
+  } else if (selection === "create") {
+    console.log("Create New Contacts");
+    CreateContacts();
+  } else if (selection === "remove") {
+    console.log("Remove Contacts");
+    RemoveContacts();
+  }
+};
+
+function displayAllContacts() {
+  let outputStr = "";
+  for (let i = 0; i < contacts.length; i++) {
+    outputStr += `<h3>${i}: ${contacts[i].name} </h3>`;
+    outputStr += `<p> ${contacts[i].email} </p>`;
+    outputStr += `<p> ${contacts[i].phone} </p>`;
+  }
+  document.getElementById("result").innerHTML = outputStr;
+};
+
+function SearchContacts() {
+  let title = prompt("Please enter the contact name you're looking for: ");
+  let index = linearSearch(contacts.name, title);
+  if (index === -1) {
+    alert("This contact is not found");
+  } else {
+    alert ("This contact is found ");
+  }
+};
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i
   }
 }
+  return -1
+};
 
-// MENU FUNCTIONS
-function displayContacts() {
-  console.log('Display Contacts');
-}
+function EditContacts() {
 
-function addContact() {
-  console.log('Add Contact');
-}
+};
 
-function removeContact() {
-  console.log('Remove Contact');
-}
-
-function displayByName() {
-  console.log('Display by Name');
-}
-
-function displayByCountry() {
-  console.log('Display by Country');
-}
+function CreateContacts() {
+  let newContact = prompt("Please enter you new contact name:");
+  let newEmail = prompt("Please enter you new contact email:");
+  let newPhone = prompt("Please enter you new contact phone:");
+  
+  contacts.push({
+    name: newContact,
+    email: newEmail,
+    phone: newPhone
+  });
+};
+   
+function RemoveContacts() {
+  let index = +prompt("Enter the contact number you want removed:");
+  contacts.splice(index, 1);
+};
